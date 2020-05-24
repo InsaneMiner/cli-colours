162674d2afc61465c35bba6afad23fbe1488d119
InsaneMiner <58346322+InsaneMiner@users.noreply.github.com>
2020-05-25 06:36:36 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "colours.h"
 int main(){
-    std::cout << bg.red << cl.reset << std::endl;
-    std::cout  << "hello" << cl.reset << std::endl;
+    std::cout << bg.red << fg.blue << "Hello World"<< cl.reset << std::endl;
     return 0;
 }
